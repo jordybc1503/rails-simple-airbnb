d30ce413fbc089a2ab3b9e3810061fb67e8c1fc1
cesar jordi <jordybc15@gmail.com>
2024-09-02 20:22:48 -0500

    Picture url column added

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -46,6 +46,6 @@ class FlatsController < ApplicationController
   private
 
   def flats_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
   end
 end
